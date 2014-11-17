d947aa38cef31c4313010e639bea6104a1091f9f
Jerad Bitner <sirkitree@gmail.com>
2014-11-16 20:54:33 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Janus VR HUD
+
+A Heads Up Display for Janus VR.
+
+1. Download `hud.js` to where you keep your room's asset files.
+1. Customize the info to display by editing the `hud_info` var in `hud.js`.
+1. Add the js file to your room with
+```html
+<AssetScript src="assets/js/hud.js" />
+```
---
+++ hud.js
@@ -0,0 +1,23 @@
+room.onLoad = function () {
+  // create our text object
+  room.createObject('Text', {
+    js_id : 'JanusVR-HUD',
+    text : 'JanusVR-HUD',
+    pos : Vector(0, 0, 0),
+    fwd : scalarMultiply(player.view_dir, -1) // face player
+  });
+};
+
+room.update = function (dt) {
+  // keep the object in the user's view
+  var console_pos = translate(player.pos, Vector(1));
+
+  // pos, xdir, ydir, zdir, fps, size, tris, userid, face, connection, address
+  // the above are already shown by hitting \ so put something else useful here
+  var hud_info = 'Whatever you like here.';
+  room.objects['JanusVR-HUD'].text = hud_info;
+
+  // follows and faces the player
+  room.objects["JanusVR-HUD"].pos = translate(translate(player.pos, player.view_dir), Vector(0, 1.40, 0));
+  room.objects["JanusVR-HUD"].fwd = scalarMultiply(player.view_dir, -1);
+};
