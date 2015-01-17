b1bd4bf47586ccdb44edb9cf0c809a44b7cda91f
sirkitree <sirkitree@gmail.com>
2015-01-16 20:08:06 -0700

    put our vector calls inside onLoad instead of update

--- hud.js
+++ hud.js
@@ -6,11 +6,15 @@ room.onLoad = function () {
     pos : Vector(0, 0, 0),
     fwd : scalarMultiply(player.view_dir, -1) // face player
   });
+
+  // create our vector references
+  var player_facing = Vector(1);
+  var hud_pos = Vector(0, 1.40, 0);
 };
 
 room.update = function (dt) {
   // keep the object in the user's view
-  var console_pos = translate(player.pos, Vector(1));
+  var console_pos = translate(player.pos, player_facing);
 
   // pos, xdir, ydir, zdir, fps, size, tris, userid, face, connection, address
   // the above are already shown by hitting \ so put something else useful here
@@ -18,6 +22,6 @@ room.update = function (dt) {
   room.objects['JanusVR-HUD'].text = hud_info;
 
   // follows and faces the player
-  room.objects["JanusVR-HUD"].pos = translate(translate(player.pos, player.view_dir), Vector(0, 1.40, 0));
+  room.objects["JanusVR-HUD"].pos = translate(translate(player.pos, player.view_dir), hud_pos);
   room.objects["JanusVR-HUD"].fwd = scalarMultiply(player.view_dir, -1);
 };
